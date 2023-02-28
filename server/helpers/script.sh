a0baf926df656670e913270bc818d00157b81727
0wx <xvnyan@gmail.com>
2023-03-01 06:50:03 +0700

    refactor(server): update script position

--- server/helpers/script.sh
+++ server/helpers/script.sh
@@ -1,12 +1,14 @@
+#!/usr/bin/env bash
+
 POSTGRES_WORK_DIR=$(pwd)/postgres
 MONGODB_WORK_DIR=$(pwd)/mongodb
 
-for folder in ../../services/*; do
+for folder in ../services/*; do
   if [ -d "$folder" ]; then
-    cd "$folder"
-
+    
     cp -r $POSTGRES_WORK_DIR/models $folder
     cp -r $POSTGRES_WORK_DIR/config $folder
+    cp -r $POSTGRES_WORK_DIR/migrations $folder
 
     cp -r $MONGODB_WORK_DIR/mongo $folder
   fi
