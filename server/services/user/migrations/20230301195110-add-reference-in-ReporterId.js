54735a19619d6139c830b7b0fdbc9f570a344145
0wx <xvnyan@gmail.com>
2023-03-02 02:57:50 +0700

    chore(server): update migration

---
+++ server/services/user/migrations/20230301195110-add-reference-in-ReporterId.js
@@ -0,0 +1,35 @@
+'use strict'
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+
+    await queryInterface.addConstraint('Reports',{
+      type: 'FOREIGN KEY',
+      fields: ['ReporterId'], 
+      references: {
+        table: 'Users',
+        field: 'id'
+      },
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE'
+    })
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+
+    await queryInterface.removeConstraint('Reports', 'Reports_ReporterId_Users_fk')
+  },
+}
