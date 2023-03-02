021e879b23bc72b2b0b04377025fe2980e7a331b
Adkasy <adityakasyidi09@gmail.com>
2023-03-03 01:36:29 +0700

    feat(client/cms-admin): initialization project

---
+++ client/cms/src/router/index.js
@@ -0,0 +1,27 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import LoginPage from '../views/LoginPage.vue'
+import RegisterPage from '../views/RegisterPage.vue'
+import HomePage from '../views/HomePage.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginPage
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: RegisterPage
+    },
+    {
+      path: '/',
+      name: 'home',
+      component: HomePage
+    }
+  ]
+})
+
+export default router
