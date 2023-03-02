5d63c6a31df311af34fc35daf46914c2238b8975
sulkhan10 <sulkhangalangsakti@gmail.com>
2023-03-03 06:32:24 +0700

    feat(client/admin): add navbar to add counselor page

--- client/cms/src/views/CounselorSubmission.vue
+++ client/cms/src/views/CounselorSubmission.vue
@@ -9,6 +9,8 @@ export default {
 </script>
 
 <template>
+      <Navbar />
+
   <section class="col-md-9 ms-sm-auto col-lg-10 px-md-4" id="company-section" style="margin: auto">
     <div
       class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom"
