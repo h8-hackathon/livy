555db592b0ca3db631c7a7c8cb8862a701ab6086
0wx <xvnyan@gmail.com>
2023-03-04 02:58:48 +0700

    feat(orchestrator): global endpoint `/login` and `verify`

--- server/orchestrator/routes/index.routes.js
+++ server/orchestrator/routes/index.routes.js
@@ -1,5 +1,9 @@
 const { Router } = require('express')
+const AuthController = require('../controllers/auth.controllers')
 
 const router = Router()
+
+router.post('/login', AuthController.login)
+router.post('/verify', AuthController.verify)
 
 module.exports = router
