9fd60ab2caea93c1365c69c5f8a7450875ad4c8c
Muhammad Jawahiruzzaman <m.jawahiruzzaman@gmail.com>
2023-03-03 22:46:20 +0700

    feat(client/conselour) : add mdi icons

--- client/conselour/src/components/Chats/ChatInput.js
+++ client/conselour/src/components/Chats/ChatInput.js
@@ -1,17 +1,14 @@
+import { mdiSend } from '@mdi/js'
+import Icon from '@mdi/react'
 import React from 'react'
 
 export default function ChatInput() {
   return (
     <form >
       <div className="flex items-center px-5 pb-6  rounded-lg h-20 ">
-        <button type="button" className="p-2 text-primary rounded-lg cursor-pointer hover:text-gray-900 hover:bg-gray-100 ">
-          <svg aria-hidden="true" className="w-9 h-9" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM7 9a1 1 0 100-2 1 1 0 000 2zm7-1a1 1 0 11-2 0 1 1 0 012 0zm-.464 5.535a1 1 0 10-1.415-1.414 3 3 0 01-4.242 0 1 1 0 00-1.415 1.414 5 5 0 007.072 0z" clipRule="evenodd"></path></svg>
-          <span className="sr-only">Add emoji</span>
-        </button>
         <textarea id="chat" rows="1" className="block mx-4 p-3 w-full text-md text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-primary focus:border-primary outline-none resize-none" placeholder="Your message..."></textarea>
         <button type="submit" className="inline-flex justify-center p-2 text-primary rounded-full cursor-pointer hover:bg-blue-">
-          <svg aria-hidden="true" className="w-9 h-9   rotate-90" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path></svg>
-          <span className="sr-only">Send message</span>
+          <Icon path={mdiSend} size={1.5} />
         </button>
       </div>
     </form>
--- client/conselour/src/components/Sidebar.js
+++ client/conselour/src/components/Sidebar.js
@@ -1,10 +1,16 @@
 import Card from "./Card";
+import Image from "next/image";
 
 export default function Sidebar() {
   return (
     <div className='w-2/6 flex flex-col bg-green-50 h-screen  '>
       <div className="p-8 ">
-        <h1 className='text-primary text-3xl font-bold'>Chats</h1>
+        <div className="flex justify-between items-center">
+          <h1 className='text-primary text-3xl font-bold'>Chats</h1>
+          <div className="relative w-11 h-11 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+            <Image src="/Image/thinking.png" height={200} width={200} />
+          </div>
+        </div>
         <div className="border-1 mt-8 border h-fit bg-white py-3 px-5 rounded-md">
           <input type="text" className="w-full h-full outline-none" />
         </div>
