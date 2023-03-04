b39e834df0d2762cdc68df48692d2cece912c9e3
sulkhan10 <sulkhangalangsakti@gmail.com>
2023-03-05 00:27:23 +0700

    fix(client/cms-admin): table head

--- client/cms/src/views/CounselorSubmission.vue
+++ client/cms/src/views/CounselorSubmission.vue
@@ -3,6 +3,8 @@ import Navbar from '../components/Navbar.vue'
 import { mapActions, mapState } from 'pinia'
 import { useCounterStore } from '../stores/counter'
 import Card from '../components/Card.vue'
+import axios from 'axios'
+let baseUrl = 'https://api.livy.chat'
 
 export default {
   props: ['counselor'],
@@ -13,7 +15,20 @@ export default {
     ...mapState(useCounterStore, ['counselors'])
   },
   methods: {
-    ...mapActions(useCounterStore, ['fetchCounselors', 'updateStatusCounselor'])
+    ...mapActions(useCounterStore, ['fetchCounselors', 'updateStatusCounselor']),
+    // acceptCounselor(id) {},
+    async acceptCounselor(id) {
+      await axios.patch(
+        baseUrl + `/counselor/${id}`,
+        {
+          headers: {
+            access_token: localStorage.access_token
+          }
+        }
+      )
+      await this.fetchCounselors()
+      await this.$router.push('/forum')
+    }
   },
   created() {
     this.fetchCounselors()
@@ -32,40 +47,51 @@ export default {
       <div
         class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom"
       >
-        <h1 class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Counselors Page</h1>
+        <h1 class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Counselor Submisson List</h1>
       </div>
       <div class="row">
         <div class="col-12">
           <table class="table align-middle">
-            <!-- Untuk Table Jobs -->
             <thead>
               <tr>
                 <th scope="col">No.</th>
+                <th scope="col">Photo</th>
                 <th scope="col">Status</th>
                 <th scope="col">Submission</th>
-                <th scope="col">Counselors Name</th>
+                <th scope="col">Counselor's Name</th>
+                <th scope="col">Counselor's Email</th>
+                <th scope="col">Counselor's DoB</th>
+                <th scope="col">Counselor's User ID</th>
                 <th scope="col">Action</th>
                 <th scope="col" width="50px"></th>
               </tr>
             </thead>
 
-            <!-- <Card v-for="(counselor, i) in counselors" :key="i" :counselor="counselor" /> -->
-
             <tbody>
               <tr v-for="(el, index) in counselors" :key="index">
                 <td>{{ ++index }}</td>
+                <td>{{ el.User.image }}</td>
                 <td>{{ el.status }}</td>
                 <td>{{ el.submissions }}</td>
+                <td>{{ el.User.name }}</td>
+                <td>{{ el.User.email }}</td>
+                <td>{{ el.User.dob }}</td>
                 <td>{{ el.UserId }}</td>
                 <td>
                   <button
                     type="button"
-                    class="btn btn-warning text-light"
-                    @click.prevent="$router.push(`/counselor-edit`)"
+                    class="btn btn-success text-light"
+                    @click.prevent="acceptCounselor(el.id)"
                   >
                     Accept
                   </button>
-                  <button type="button" class="btn btn-danger m-2">Delete</button>
+                  <button
+                    type="button"
+                    class="btn btn-danger m-2"
+                    @click.prevent="rejectCounselor(el.id)"
+                  >
+                    Delete / Reject
+                  </button>
                 </td>
               </tr>
             </tbody>
