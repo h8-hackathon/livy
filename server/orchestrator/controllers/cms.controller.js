72bd622cb4e524d18b37bcda69496d1aea06c125
0wx <xvnyan@gmail.com>
2023-03-04 20:19:29 +0700

    feat(orchestrator): finish creating cms endpoint

--- server/orchestrator/controllers/cms.controller.js
+++ server/orchestrator/controllers/cms.controller.js
@@ -103,6 +103,197 @@ class CMSController {
       next(error);
     }
   }
+
+  static async getAllCounselor(req, res, next) {
+    try {
+      const { data: counselors } = await adminAPI.get("/counselors");
+      res.status(200).json(counselors.filter((el) => el.status === "pending"));
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async acceptCounselor(req, res, next) {
+    try {
+      const { id } = req.params;
+      await userAPI.put(`/counselor/${id}/submissions`, { status: "accepted" });
+      res.status(200).json({ message: "data successfully updated" });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async getForumReport(req, res, next) {
+    try {
+      const { data: reports } = await adminAPI.get("/reports");
+      const posts = [];
+      const comments = [];
+      for (let i = 0; i < reports.length; i++) {
+        const element = reports[i];
+        if (element.commentId) {
+          comments.push(element);
+        } else {
+          posts.push(element);
+        }
+      }
+      const groupedPosts = {};
+      const groupedComments = {};
+      for (let i = 0; i < posts.length; i++) {
+        const element = posts[i];
+        if (groupedPosts[element.postId]) {
+          groupedPosts[element.postId].push(element);
+        } else {
+          groupedPosts[element.postId] = [element];
+        }
+      }
+
+      for (let i = 0; i < comments.length; i++) {
+        const element = comments[i];
+        if (groupedComments[element.commentId]) {
+          groupedComments[element.commentId].push(element);
+        } else {
+          groupedComments[element.commentId] = [element];
+        }
+      }
+
+      const postIds = Object.keys(groupedPosts);
+      const commentIds = Object.keys(groupedComments);
+      const postReports = [];
+      const commentReports = [];
+      for (let i = 0; i < postIds.length; i++) {
+        const element = postIds[i];
+        const { data: post } = await forumAPI.get(`/posts/${element}`);
+        const { data: user } = await userAPI.get(`/users/${post.UserId}`);
+        post.author = user;
+        post.reports = groupedPosts[element].length;
+        postReports.push(post);
+      }
+
+      for (let i = 0; i < commentIds.length; i++) {
+        const element = commentIds[i];
+        const { data: comment } = await forumAPI.get(`/comments/${element}`);
+        const { data: user } = await userAPI.get(`/users/${comment.UserId}`);
+        comment.author = user;
+        comment.reports = groupedComments[element].length;
+        commentReports.push(comment);
+      }
+
+      res.status(200).json({ postReports, commentReports });
+    } catch (error) {
+      next(error);
+    }
+  }
+        
+
+  static async rejectCounselor(req, res, next) {
+    try {
+      const { id } = req.params;
+      await userAPI.delete(`/users/${id}`);
+      res.status(200).json({ message: "data successfully updated" });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async deleteReportedPost(req, res, next) {
+    try {
+      const { postId } = req.params;
+      await forumAPI.delete(`/posts/${postId}`);
+      await adminAPI.delete(`/reports/post/${postId}`);
+      res.status(200).json({ message: "data successfully deleted" });
+    } catch (error) {
+      next(error);
+    }
+  }
+  static async ignoreReportedPost(req, res, next) {
+    try {
+      const { postId } = req.params;
+      await adminAPI.delete(`/reports/post/${postId}`);
+      res.status(200).json({ message: "data successfully deleted" });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async deleteReportedComment(req, res, next) {
+    try {
+      const { commentId } = req.params;
+      await forumAPI.delete(`/comments/${commentId}`);
+      await adminAPI.delete(`/reports/comment/${commentId}`);
+      res.status(200).json({ message: "data successfully deleted" });
+    } catch (error) {
+      next(error);
+    }
+  }
+  static async ignoreReportedComment(req, res, next) {
+    try {
+      const { commentId } = req.params;
+      await adminAPI.delete(`/reports/comment/${commentId}`);
+      res.status(200).json({ message: "data successfully deleted" });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  // get /users from userAPI with type admin
+  static async getAllAdmin(req, res, next) {
+    try {
+      const { data: admins } = await userAPI.get("/users?type=admin");
+      res.status(200).json(admins);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  // post /users/admin from userAPI
+  static async createAdmin(req, res, next) {
+    try {
+      const { name, email } = req.body;
+      const { data: admin } = await userAPI.post("/users/admin", {
+        name,
+        email,
+      });
+      res.status(201).json(admin);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  // get /users/:id from userAPI with type admin
+  static async getAdminById(req, res, next) {
+    try {
+      const { id } = req.params;
+      const { data: admin } = await userAPI.get(`/users/${id}`);
+      res.status(200).json(admin);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  // put /users/:id from userAPI with type admin
+  static async updateAdmin(req, res, next) {
+    try {
+      const { id } = req.params;
+      const { data: admin } = await userAPI.put(`/users/${id}`, req.body);
+      res.status(200).json(admin);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  // delete /users/:id from userAPI with type admin
+  static async deleteAdmin(req, res, next) {
+    try {
+      const { id } = req.params;
+      const { data: admin } = await userAPI.delete(`/users/${id}`);
+      res.status(200).json(admin);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+
+
 }
 
 module.exports = CMSController;
