67bde42572b4118c58c39cee6f69baa0d104b409
0wx <xvnyan@gmail.com>
2023-03-06 05:14:54 +0700

    feat(orchestrator): chat with livy endpoint

--- server/orchestrator/controllers/client.controllers.js
+++ server/orchestrator/controllers/client.controllers.js
@@ -1,4 +1,4 @@
-const { adminAPI, scheduleAPI, userAPI } = require('../helpers/axios')
+const { adminAPI, scheduleAPI, userAPI, chatAPI } = require('../helpers/axios')
 const redis = require('../helpers/redis')
 
 const HOME = 'client:home'
@@ -53,8 +53,36 @@ class ClientController {
       const { data: counselors } = await adminAPI.get('/counselors')
       await redis.set(COUNSELOR, JSON.stringify(counselors))
       await redis.expire(COUNSELOR, 300)
-      
+
       res.status(200).json(counselors.filter((el) => el.status !== 'pending'))
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  static async getChatWithLivy(req, res, next) {
+    try {
+      const { access_token } = req.headers
+      const { data: user } = await userAPI.get('/verify', { access_token })
+      const { data: chat } = await chatAPI.get('/chat/' + user.id + '/livy')
+
+      res.status(200).json(chat)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  static async chatWithLivy(req, res, next) {
+    try {
+      const { access_token } = req.headers
+      const { text } = req.body
+      const { data: user } = await userAPI.get('/verify', { access_token })
+      const { data: reply } = await chatAPI.post('/chat/' + user.id + '/livy', {
+        text,
+        sender: user,
+      })
+
+      res.status(200).json(reply)
     } catch (error) {
       next(error)
     }
--- server/orchestrator/routes/client.routes.js
+++ server/orchestrator/routes/client.routes.js
@@ -4,5 +4,10 @@ const ClientController = require('../controllers/client.controllers')
 const router = Router()
 
 router.get('/home', ClientController.getHome)
+router.get('/counselors', ClientController.getAllCounselor)
+router.get('/schedule', ClientController.getSchedule)
+
+router.get('/chatLivy', ClientController.getChatWithLivy)
+router.post('/chatLivy', ClientController.chatWithLivy)
 
 module.exports = router
