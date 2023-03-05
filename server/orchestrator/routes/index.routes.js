e3613f34bf8abdd44f6b76fb9a8e3c5c667fe49d
0wx <xvnyan@gmail.com>
2023-03-06 04:16:57 +0700

    feat(orchestrator): add error middleware

--- server/orchestrator/routes/index.routes.js
+++ server/orchestrator/routes/index.routes.js
@@ -1,6 +1,7 @@
 const { Router } = require('express')
 const AuthController = require('../controllers/auth.controllers')
 const router = Router()
+const errorMiddleware = require('../middlewares/error.middlewares')
 
 const cmsRouter = require('./cms.routes')
 const client = require('./client.routes')
@@ -11,4 +12,5 @@ router.post('/verify', AuthController.verify)
 router.use('/cms', cmsRouter)
 router.use('/client', client)
 
+router.use(errorMiddleware)
 module.exports = router
