c05af4fd9e931fbe68eb6af546f032e7c751bd93
0wx <xvnyan@gmail.com>
2023-03-05 09:10:25 +0700

    feat(orchestrator): init socket.io

--- server/orchestrator/listeners/index.js
+++ server/orchestrator/listeners/index.js
@@ -4,6 +4,21 @@ const { Server } = require('socket.io')
 const http = require('http')
 const server = http.createServer(app)
 
-const io = new Server(server)
+const io = new Server(server, {
+  cors: '*',
+})
+
+io.on('connection', (socket) => {
+  socket.on('join', (room, cb) => {
+    socket.join(room)
+    if(typeof cb === 'function') cb()
+  })
+
+  socket.on('message', (room, message) => {
+    io.to(room).emit('message', message)
+  })
+
+
+})
 
 module.exports = { io, server }
