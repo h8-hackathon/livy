d6e2e366fff6f6375177479f06ab874483451f21
Muhammad Jawahiruzzaman <m.jawahiruzzaman@gmail.com>
2023-03-06 17:33:39 +0700

    feat(client/counselor): edit profile

--- client/conselour/src/components/ProfileDetail.js
+++ client/conselour/src/components/ProfileDetail.js
@@ -1,3 +1,4 @@
+import { api } from "@/helpers";
 import { useCounselor } from "@/hooks/useCounselor";
 import { mdiArrowLeft, mdiPower } from "@mdi/js";
 import Icon from "@mdi/react";
@@ -31,6 +32,13 @@ export default function ProfileDetail() {
     else setForm(counselor)
     setEdit(!edit)
   }
+
+  const submit = async (e) => {
+    e.preventDefault();
+
+   const res =  await api.put('/counselor', { ...form, dob: new Date(form.dob).toISOString() })
+    console.log(res.data)
+  }
   return (
     <div className="px-8 p-10 overflow-auto">
       <div className="flex items-center justify-between">
@@ -48,7 +56,7 @@ export default function ProfileDetail() {
         <div className="relative mx-auto w-48 h-48 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
           <Image loader={() => counselor.image} alt="profile-img" src={counselor.image} height={200} width={200} />
         </div>
-        <form className="space-y-8 mt-10 text-center">
+        <form onSubmit={submit} className="space-y-8 mt-10 text-center">
           <div className="space-x-2">
             <Link href="/profile/activity" className="bg-primary px-5 py-2 rounded text-xs text-white">Edit Activity</Link>
             <button type="button" onClick={handleEdit} className="bg-slate-200 px-3 py-2 rounded text-xs">
@@ -61,22 +69,22 @@ export default function ProfileDetail() {
           </div>
           <div className="space-y-1">
             <p className="text-primary">Email</p>
-            {edit ? <input name="email" onChange={inputHandler} value={form.email} className="px-3 py-2 text-xs w-3/4 outline-none" type="email" required  /> : <p className="text-base">{counselor.email || 'Not set'}</p>}
+            {edit ? <input name="email" disabled onChange={inputHandler} value={form.email} className="px-3 py-2 text-xs w-3/4 outline-none" type="email" required /> : <p className="text-base">{counselor.email || 'Not set'}</p>}
           </div>
           <div className="space-y-1">
             <p className="text-primary">Gender</p>
             {
               edit ?
-                <select name="gender" onChange={inputHandler} value={form.gender} className="px-3 py-2 text-xs w-3/4 outline-none" type="text" >
-                  <option value={null} disabled>Not set</option>
-                  <option value="male">Male</option>
-                  <option value="female">Female</option>
+                <select name="gender" onChange={inputHandler} value={form.gender || ""} className="px-3 py-2 text-xs w-3/4 outline-none" type="text" >
+                  <option  disabled value="" >Not set</option>
+                  <option value="M">Male</option>
+                  <option value="F">Female</option>
                 </select>
                 : <p className="text-base">{counselor.gender || 'Not set'}</p>}
           </div>
           <div className="space-y-4">
             <p className="text-primary">Date of birthday</p>
-            {edit ? <input name="dob" onChange={inputHandler} value={form.dob} className="px-3 py-2 text-xs w-3/4 outline-none" type="date"  required /> : <p className="text-base">{counselor.dob || 'Not set'}</p>}
+            {edit ? <input name="dob" onChange={inputHandler} value={form.dob} className="px-3 py-2 text-xs w-3/4 outline-none" type="date" /> : <p className="text-base">{counselor.dob || 'Not set'}</p>}
           </div>
           {
             edit && <button type="submit" className="bg-primary text-white px-3 py-2 rounded text-xs w-3/4">Submit</button>
