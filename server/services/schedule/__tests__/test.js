2e0f3e5ec84526e89197b37c7211e103181998d2
sulkhan10 <sulkhangalangsakti@gmail.com>
2023-03-07 23:01:13 +0700

    feat(schedule/test) : Successfully post schedule by user id

--- server/services/schedule/__tests__/test.js
+++ server/services/schedule/__tests__/test.js
@@ -32,20 +32,18 @@ beforeAll(async () => {
       role: "counselor",
       helpful: 4,
     });
-    await Schedule.create(
-      {
-        status: "paid",
-        CounselorId: 2,
-        UserId: 1,
-        time: new Date(),
-        note: "apayaaa",
-        session: "2023-03-07 14:59:05.400 +0700",
-        expPaymentUrl: "2023-03-05 14:59:05.400 +0700",
-        paymentUrl: "https://checkout-staging.xendit.co/web/64071c9332260d834f1136ec",
-        rating: 5
-      }
-    )
-
+    await Schedule.create({
+      status: "paid",
+      CounselorId: 2,
+      UserId: 1,
+      time: new Date(),
+      note: "apayaaa",
+      session: "2023-03-07 14:59:05.400 +0700",
+      expPaymentUrl: "2023-03-05 14:59:05.400 +0700",
+      paymentUrl:
+        "https://checkout-staging.xendit.co/web/64071c9332260d834f1136ec",
+      rating: 5,
+    });
   } catch (error) {
     console.log(error, "ini errornya");
   }
@@ -70,43 +68,76 @@ afterAll(async () => {
       restartIdentity: true,
     }
   );
-
 });
 
 describe("for schedule", () => {
   // schedule read by counselor id 200
   it("Successfully read schedule by counselor id", async () => {
-    let counselorId = 2
-    const response = await request(app).get("/schedules/counselor/"+counselorId);
+    let counselorId = 2;
+    const response = await request(app).get(
+      "/schedules/counselor/" + counselorId
+    );
     expect(response.status).toBe(200);
-    console.log(response.body)
+    console.log(response.body);
     expect(response.body).toBeInstanceOf(Array);
     expect(response.body[0]).toHaveProperty("UserId", expect.any(Number));
     expect(response.body[0]).toHaveProperty("CounselorId", expect.any(Number));
     expect(response.body[0]).toHaveProperty("status", expect.any(String));
     expect(response.body[0]).toHaveProperty("paymentUrl", expect.any(String));
-    expect(response.body[0]).toHaveProperty("expPaymentUrl", expect.any(String));
+    expect(response.body[0]).toHaveProperty(
+      "expPaymentUrl",
+      expect.any(String)
+    );
     expect(response.body[0].User).toBeInstanceOf(Object);
     expect(response.body[0].User).toHaveProperty("name", expect.any(String));
   });
 
   // schedule read by user id 200
-  it.only("Successfully read schedule by user id", async () => {
-    let userId = 1
-    const response = await request(app).get("/schedules/user/"+userId);
+  it("Successfully read schedule by user id", async () => {
+    let userId = 1;
+    const response = await request(app).get("/schedules/user/" + userId);
     expect(response.status).toBe(200);
-    console.log(response.body)
+    // console.log(response.body);
     expect(response.body).toBeInstanceOf(Array);
     expect(response.body[0]).toHaveProperty("UserId", expect.any(Number));
     expect(response.body[0]).toHaveProperty("CounselorId", expect.any(Number));
     expect(response.body[0]).toHaveProperty("status", expect.any(String));
     expect(response.body[0]).toHaveProperty("paymentUrl", expect.any(String));
-    expect(response.body[0]).toHaveProperty("expPaymentUrl", expect.any(String));
+    expect(response.body[0]).toHaveProperty(
+      "expPaymentUrl",
+      expect.any(String)
+    );
     expect(response.body[0].Counselor).toBeInstanceOf(Object);
-    expect(response.body[0].Counselor).toHaveProperty("name", expect.any(String));
+    expect(response.body[0].Counselor).toHaveProperty(
+      "name",
+      expect.any(String)
+    );
   });
 
-
-
-
+  // post schedule by user id 200
+  it.only("Successfully post schedule by user id", async () => {
+    let userId = 1;
+    let CounselorId = 2;
+    let time = new Date();
+    let note = 'add schedule'
+    const response = await request(app)
+      .post("/schedules/user/" + userId)
+      .send({ CounselorId, time, note });
+    expect(response.status).toBe(201);
+    console.log(response.body);
+    expect(response.body).toBeInstanceOf(Object);
+    expect(response.body.response).toHaveProperty("status", expect.any(String));
+    expect(response.body.response).toHaveProperty("UserId", expect.any(Number));
+    expect(response.body.response).toHaveProperty("session", expect.any(String));
+    expect(response.body.response).toHaveProperty("note", expect.any(String));
+    expect(response.body.response).toHaveProperty("CounselorId", expect.any(Number));
+    expect(response.body.response).toHaveProperty(
+      "paymentUrl",
+      expect.any(String)
+    );
+    expect(response.body.response).toHaveProperty(
+      "expPaymentUrl",
+      expect.any(String)
+    );
+  });
 });
