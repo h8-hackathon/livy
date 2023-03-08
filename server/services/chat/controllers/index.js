86f5e7f8c08ff42695be4f99007c21b9b0845253
sulkhan10 <sulkhangalangsakti@gmail.com>
2023-03-08 22:13:03 +0700

    fix(chat/test) : coverage branch 64percent

--- server/services/chat/controllers/index.js
+++ server/services/chat/controllers/index.js
@@ -1,138 +1,136 @@
-/* istanbul ignore file */
-
-const askChatGpt = require('../chatgpt')
-const { convertChatToPrompts } = require('../helpers')
-const Chat = require('../mongo/models/Chat')
+const askChatGpt = require("../chatgpt");
+const { convertChatToPrompts } = require("../helpers");
+const Chat = require("../mongo/models/Chat");
 
 class Controller {
   static async listChat(req, res, next) {
     try {
-      const { userId } = req.params
+      const { userId } = req.params;
 
       let chat = await Chat.findOne({
         UserId: +userId,
-      })
+      });
 
-      if (!chat) throw { message: 'Not Found' }
+      if (!chat) throw { message: "Not Found" };
 
-      res.status(200).json(chat)
+      res.status(200).json(chat);
     } catch (error) {
-      next(error)
+      next(error);
     }
   }
 
   static async createChat(req, res, next) {
     try {
-      const { counselorId, chat } = req.body
-      const { userId } = req.params
+      const { counselorId, chat } = req.body;
+      const { userId } = req.params;
 
       let chats = await Chat.findOne({
         UserId: +userId,
         CounselorId: +counselorId,
-      })
+      });
 
-      if (!chats)
-        chats = { UserId: +userId, chats: [], CounselorId: counselorId }
-
-      chats.chats.push({ ...chat, time: new Date() })
+      if (!chats) {
+        chats = { UserId: +userId, chats: [], CounselorId: counselorId };
+      } else {
+        chats.chats.push({ ...chat, time: new Date() });
+      }
 
       await Chat.updateOne(
         { UserId: +userId, CounselorId: +counselorId },
         { $set: { ...chats } },
         { upsert: true }
-      )
+      );
 
-      res.status(201).json(chats)
+      res.status(201).json(chats);
     } catch (error) {
-      next(error)
+      next(error);
     }
   }
 
   static async chatWithLivy(req, res, next) {
     try {
-      const { text, sender } = req.body
-      const { userId } = req.params
+      const { text, sender } = req.body;
+      const { userId } = req.params;
 
       let chats = await Chat.findOne({
         UserId: +userId,
         CounselorId: null,
-      })
+      });
 
       if (!chats) {
-        chats = { UserId: +userId, chats: [], CounselorId: null }
+        chats = { UserId: +userId, chats: [], CounselorId: null };
       } else {
-        chats.chats.push({ text, time: new Date(), sender })
+        chats.chats.push({ text, time: new Date(), sender });
       }
 
-      const response = await askChatGpt(convertChatToPrompts(chats.chats))
-      chats.chats.push({ text: response.choices[0].text, time: new Date() })
+      const response = await askChatGpt(convertChatToPrompts(chats.chats));
+      chats.chats.push({ text: response.choices[0].text, time: new Date() });
 
       await Chat.updateOne(
         { UserId: +userId, CounselorId: null },
         { $set: { ...chats } },
         { upsert: true }
-      )
+      );
 
-      res.status(200).json({ message: response.choices[0].text })
+      res.status(200).json({ message: response.choices[0].text });
     } catch (error) {
-      next(error)
+      next(error);
     }
   }
 
-  static async getChatWithLivy(req, res, next) {
+  static async getChatWithLivy(req, res) {
     try {
-      const { userId } = req.params
+      const { userId } = req.params;
 
       let chats = await Chat.findOne({
         UserId: +userId,
         CounselorId: null,
-      })
+      });
 
       if (!chats) {
-        chats = { UserId: +userId, chats: [], CounselorId: null }
+        chats = { UserId: +userId, chats: [], CounselorId: null };
       }
 
-      res.status(200).json(chats)
+      res.status(200).json(chats);
     } catch (error) {
-      next(error)
+      next(error);
     }
   }
 
   static async listChatByCounselor(req, res, next) {
     try {
-      const { counselorId } = req.params
+      const { counselorId } = req.params;
 
       let chat = await Chat.find({
         CounselorId: +counselorId,
-      }).toArray()
+      }).toArray();
 
-      if (!chat) throw { message: 'Not Found' }
+      if (!chat) throw { message: "Not Found" };
 
-      res.status(200).json(chat)
+      res.status(200).json(chat);
     } catch (error) {
-      next(error)
+      next(error);
     }
   }
 
   static async listChatByUserAndCounselor(req, res, next) {
     try {
-      const { counselorId, userId } = req.params
+      const { counselorId, userId } = req.params;
 
       let chat = await Chat.findOne({
         CounselorId: +counselorId,
         UserId: +userId,
-      })
+      });
 
       if (!chat) {
-        res.json({ UserId: +userId, chats: [], CounselorId: +counselorId })
-        return
+        chat = { UserId: +userId, chats: [], CounselorId: +counselorId }
       }
 
-      res.status(200).json(chat)
+      res.status(200).json(chat);
     } catch (error) {
-      next(error)
+      next(error);
     }
   }
 }
 
-module.exports = Controller
+module.exports = Controller;
